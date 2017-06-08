6e166c4fd0f581933e3bccc11920ce526b452b6e
JASMIN ROSE LIM <jrlim13@yahoo.com>
2017-06-08 16:15:15 +0800

    Changes
    > Changed the way the database selects an account number. (Increments an account number by 1)
    > The functions are still kept as a comment just in case

--- IT131_Queries.sql
+++ IT131_Queries.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE Project;
 USE Project;
 
+--> Still kept just in case. <--
+
 --CREATE SEQUENCE Account_Number_Seq
 --	AS INT
 --	START WITH 1000000000000000
@@ -14,28 +16,28 @@ USE Project;
 --	INCREMENT BY 1
 --	NO CYCLE;
 
-CREATE VIEW randomize 
-AS 
-SELECT 10000000 + FLOOR(99999999*RAND(CONVERT(varbinary, NEWID()))) AS acc_id, 
-		100000 + FLOOR(999999*RAND(CONVERT(varbinary, NEWID()))) AS trans_id
+--CREATE VIEW randomize 
+--AS 
+--SELECT 10000000 + FLOOR(99999999*RAND(CONVERT(varbinary, NEWID()))) AS acc_id, 
+--		100000 + FLOOR(999999*RAND(CONVERT(varbinary, NEWID()))) AS trans_id
 
-CREATE FUNCTION ReturnRandomId(@choice int)
-RETURNS int
-AS
-BEGIN
-	DECLARE @id_num int
-	DECLARE @random_num int
+--CREATE FUNCTION ReturnRandomId(@choice int)
+--RETURNS int
+--AS
+--BEGIN
+--	DECLARE @id_num int
+--	DECLARE @random_num int
 
-	IF @choice = 1 --> Randomize Account Number
-	BEGIN
-		SET @random_num = (select acc_id from randomize)
-		SET @id_num = CONVERT(int, @random_num)
+--	IF @choice = 1 --> Randomize Account Number
+--	BEGIN
+--		SET @random_num = (select acc_id from randomize)
+--		SET @id_num = CONVERT(int, @random_num)
 
-		IF @id_num / 100000000 != 0
-		BEGIN
-			SET @id_num = dbo.ReturnRandomId(1)
-		END
-	END
+--		IF @id_num / 100000000 != 0
+--		BEGIN
+--			SET @id_num = dbo.ReturnRandomId(1)
+--		END
+--	END
 	--IF @choice = 2
 	--BEGIN
 	--	SET @random_num = (select trans_id from randomize) --> Randomize Transaction Number
@@ -47,11 +49,11 @@ BEGIN
 	--	END
 	--END
 
-	RETURN @id_num
-END
+--	RETURN @id_num
+--END
 
 CREATE TABLE Bank_Account(
-	Account_Number int PRIMARY KEY NOT NULL,
+	Account_Number int IDENTITY(10000000,1) PRIMARY KEY NOT NULL,
 	Balance decimal(20,2) NOT NULL,
 	First_Name varchar(30),
 	Last_Name varchar(30),
@@ -66,9 +68,9 @@ CREATE TABLE Transaction_History(
 	Account_Number bigint NOT NULL
 );
 
-INSERT INTO Bank_Account VALUES (dbo.ReturnRandomId(1), 10000.96, 'Nath', 'Aguas', '123456');
-INSERT INTO Bank_Account VALUES (dbo.ReturnRandomId(1), 14482.10, 'John', 'Doe', '647282');
-INSERT INTO Bank_Account VALUES (dbo.ReturnRandomId(1), 200125.53, 'Maria', 'Makiling', '042168');
+INSERT INTO Bank_Account VALUES (10000.96, 'Nath', 'Aguas', '123456');
+INSERT INTO Bank_Account VALUES (14482.10, 'John', 'Doe', '647282');
+INSERT INTO Bank_Account VALUES (200125.53, 'Maria', 'Makiling', '042168');
 
 SELECT * FROM Bank_Account;
 SELECT * FROM Transaction_History;
